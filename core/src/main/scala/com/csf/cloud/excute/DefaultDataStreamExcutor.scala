db96164a37943013f84f008c2f482fa97f383448
soledede.weng(翁本珏) <soledede.weng@sh-ad.chinascopefinancial.com>
2016-07-11 15:45:25 +0800

    pom

--- core/src/main/scala/com/csf/cloud/excute/DefaultDataStreamExcutor.scala
+++ core/src/main/scala/com/csf/cloud/excute/DefaultDataStreamExcutor.scala
@@ -8,12 +8,13 @@ import com.csf.cloud.util.BizException
   * Created by soledede.weng on 2016/7/7.
   */
 private[cloud] class DefaultDataStreamExcutor extends DataStreamExcutor[String] {
-  override def service(data: util.ArrayList[String]): Unit = {
+  override def service(data: util.ArrayList[String]): java.util.ArrayList[String] = {
     try {
       val seeData = data
       println(s"Stream Data coming ${seeData.toString}")
     } catch {
       case e: Exception => throw new BizException("data stream faield")
     }
+    null
   }
 }
